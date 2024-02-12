b99b6ee0347a880086967896dc220b0bb7ddc90b
Shakir Riaz <shakir@MacBook-Pro.local>
2024-02-12 15:23:13 +0530

    m

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -17,6 +17,8 @@ class MostActiveCookie:
         self.DATE = ""
 
     def begin(self, args):
+
+        # check whether the length of args is 5 or not.
         if len(args) != 5:
             print(self.ERROR_MSG_INCORRECT_USAGE)
             return
@@ -25,19 +27,24 @@ class MostActiveCookie:
         self.FILENAME = args[2]
         self.DATE_FLAG = args[3]
         self.DATE = args[4]
-
+        # if flags are not valid, then return.
         if not self.are_flags_valid():
             return
 
         try:
-            same_date_cookies_freq = self.parse_csv(self.FILENAME)
-            self.print_most_common_cookie(same_date_cookies_freq)
+            # parse the csv file.
+            same_date_cookies_freq = self.parse_csv(self.FILENAME) # parse_csv returns cookie frequency map.
+            self.print_most_common_cookie(same_date_cookies_freq) # print the most common cookies.
         except FileNotFoundError:
             print(f"Error: File {self.FILENAME} not found.")
         except Exception as e:
             print(str(e))
     
     def are_flags_valid(self):
+        # This method checks whether the file and date flags are correct or not 
+        # and displays the error message accordingly if incorrect.
+        # It returns True/False according to whether the flags are correct or not.
+
         if self.FILE_FLAG != "-f" and self.DATE_FLAG != "-d":
             print(self.ERROR_MSG_INCORRECT_FILE_DATE_FLAGS)
             return False
@@ -53,6 +60,10 @@ class MostActiveCookie:
         return True
     
     def parse_csv(self, filename):
+        # This method parses the csv file and also handle edge cases like File Not Found 
+        # or Incorrect File Format and raises exception accordingly. 
+        # It returns the cookie frequency map.
+
         parsed_csv = {}
 
         try:
@@ -61,13 +72,15 @@ class MostActiveCookie:
                 next(reader)  # skip header
 
                 for line in reader:
+                    # check whether the file is in correct format or not.
                     if len(line) != 2:
                         raise Exception("Error: The file is not in the correct format.")
-
+                    # get the CookieIDTimePair object
                     cookie_id_date_pair_obj = self.parse_cookie_entry(line)
-
+                    # check whether the date is same or not as the specified date.
                     if self.is_same_date(datetime.strptime(self.DATE, self.formatter).date(), cookie_id_date_pair_obj.get_date()):
                         cookie_id = cookie_id_date_pair_obj.get_cookie_id()
+                        # update the frequency map.
                         parsed_csv[cookie_id] = parsed_csv.get(cookie_id, 0) + 1
 
         except FileNotFoundError:
@@ -79,20 +92,27 @@ class MostActiveCookie:
         return input_date == target_date
 
     def parse_cookie_entry(self, line):
+        # converts line into a CookieIDTimePair object with line[0] as cookieId 
+        # and line[1] as date and returns that object.
+
         cookie_date = datetime.strptime(line[1][:10], self.formatter).date()
         cookie_id_date_pair_obj = CookieIDTimePair(line[0], cookie_date)
         return cookie_id_date_pair_obj
 
     def print_most_common_cookie(self, same_date_cookies_freq):
+        # this method accepts a cookie frequency map and fetches, prints, 
+        # and returns the most frequently used cookie(s).
+
         most_common_cookies = set()
         max_value = 0
+        # get the frequency of most frequently used cookie(s)
+        for cookie_id, freq in same_date_cookies_freq.items():
+            if freq > max_value:
+                max_value = freq
 
+        # Fetch the most frequently used cookie(s)
         for cookie_id, freq in same_date_cookies_freq.items():
             if freq == max_value:
-                most_common_cookies.add(cookie_id)
-            elif freq > max_value:
-                most_common_cookies.clear()
-                max_value = freq
                 most_common_cookies.add(cookie_id)
 
         for cookie in most_common_cookies:
--- most_active_cookie_tests.py
+++ most_active_cookie_tests.py
@@ -9,47 +9,56 @@ class TestMostActiveCookie(unittest.TestCase):
         self.most_active_cookie = MostActiveCookie()
 
     def test_correct_file_date_flags(self):
+        # Test correct file and date flags.
         self.most_active_cookie.FILE_FLAG = "-f"
         self.most_active_cookie.DATE_FLAG = "-d"
         self.assertTrue(self.most_active_cookie.are_flags_valid())
 
     def test_incorrect_file_flag(self):
+        # Test incorrect file flag.
         self.most_active_cookie.FILE_FLAG = "-x"
         self.most_active_cookie.DATE_FLAG = "-d"
         self.assertFalse(self.most_active_cookie.are_flags_valid())
 
     def test_incorrect_date_flag(self):
+        # Test incorrect date flag.
         self.most_active_cookie.FILE_FLAG = "-f"
         self.most_active_cookie.DATE_FLAG = "-x"
         self.assertFalse(self.most_active_cookie.are_flags_valid())
 
     def test_incorrect_file_date_flags(self):
+        # Test incorrect file and date flags.
         self.most_active_cookie.FILE_FLAG = "-x"
         self.most_active_cookie.DATE_FLAG = "-x"
         self.assertFalse(self.most_active_cookie.are_flags_valid())
 
     def test_is_same_date_false(self):
+        # Test is_same_date() on True case.
         d1 = date(2021, 1, 1)
         d2 = date(2021, 1, 2)
         self.assertFalse(self.most_active_cookie.is_same_date(d1, d2))
 
     def test_is_same_date_true(self):
+        # Test is_same_date() on False case.
         d1 = date(2021, 1, 1)
         d2 = date(2021, 1, 1)
         self.assertTrue(self.most_active_cookie.is_same_date(d1, d2))
 
     def test_parse_cookie_entry_true(self):
+        # Test parse_cookie_entry() on True case.
         values = ["A", "2020-01-01"]
         mock = CookieIDTimePair("A", date(2020, 1, 1))
         self.assertEqual(self.most_active_cookie.parse_cookie_entry(values), mock)
         self.assertIsNotNone(self.most_active_cookie.parse_cookie_entry(values))
 
     def test_parse_cookie_entry_false(self):
+        # Test parse_cookie_entry() on False case.
         values = ["A", "2020-01-02"]
         mock = CookieIDTimePair("A", date(2020, 1, 1))
         self.assertNotEqual(self.most_active_cookie.parse_cookie_entry(values), mock)
 
     def test_parse_csv_correct_result(self):
+        # Test parse_csv() on correct result.
         self.most_active_cookie.FILENAME = "cookie_log.csv"
         self.most_active_cookie.formatter = "%Y-%m-%d"
         self.most_active_cookie.DATE = "2018-12-09"
@@ -57,6 +66,7 @@ class TestMostActiveCookie(unittest.TestCase):
         self.assertEqual(self.most_active_cookie.parse_csv(self.most_active_cookie.FILENAME), expected_result)
 
     def test_parse_csv_incorrect_result(self):
+        # Test parse_csv() on incorrect result.
         self.most_active_cookie.FILENAME = "cookie_log.csv"
         self.most_active_cookie.formatter = "%Y-%m-%d"
         self.most_active_cookie.DATE = "2018-12-09"
@@ -64,17 +74,20 @@ class TestMostActiveCookie(unittest.TestCase):
         self.assertNotEqual(self.most_active_cookie.parse_csv(self.most_active_cookie.FILENAME), expected_result)
 
     def test_parse_csv_file_not_found(self):
+        # Test parse_csv() on File Not Found case.
         filename = "nonexistent_file.csv"
         with self.assertRaises(FileNotFoundError):
             self.most_active_cookie.parse_csv(filename)
     
     def test_print_most_common_cookie_true(self):
+        # Test print_most_common_cookie() on correct result.
         map_data = {"A": 4, "B": 3, "C": 2, "D": 4}
         ret = self.most_active_cookie.print_most_common_cookie(map_data)
         mock = {"A", "D"}
         self.assertSetEqual(ret, mock)
 
     def test_print_most_common_cookie_false(self):
+        # Test print_most_common_cookie() on incorrect result.
         map_data = {"A": 4, "B": 3, "C": 2, "D": 4}
         ret = self.most_active_cookie.print_most_common_cookie(map_data)
         mock = {"D"}
